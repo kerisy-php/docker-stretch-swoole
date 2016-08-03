84d160c4c05bfd9c2058f8111ebdf6d3167753ad
zoujiaqing <zoujiaqing@gmail.com>
2016-08-03 07:05:45 -0400

    Run kerisy is successed

--- app/run-docker.sh
+++ app/run-docker.sh
@@ -1,4 +1,4 @@
 
 WEBSITE_PATH=$(cd "$(dirname "$0")"; pwd)
-docker run -v $WEBSITE_PATH:/data/www/website/htdocs/ -p 8888:8888 -it stretch-swoole
+docker run -v $WEBSITE_PATH:/data/www/website/htdocs/:ro -p 8888:8888 -d stretch-swoole
 
